a45d1ea4f489793401fe4be47b16c7c98c25868f
Uwumuhire Rene <reneuwumuhire@gmail.com>
2023-12-08 11:19:05 +0200

    fix: disabling what is not working and fixing sizes

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Header() {
-	return <header>Lerna is the original monoreddpo tool! 22New Changes</header>;
+	return <header>Lerna is the original monoreddpo tool! 22rNew Changes</header>;
 }
